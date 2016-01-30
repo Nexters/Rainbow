0b31d45b3e6a0db0a96663be3e081058b593e0e0
Soyoon <soyoon0918@gmail.com>
2016-01-30 17:05:14 +0900

    push test

---
+++ RainbowCouple/app/src/main/java/com/nexters/rainbow/rainbowcouple/graph/GraphActivity.java
@@ -0,0 +1,7 @@
+package com.nexters.rainbow.rainbowcouple.graph;
+
+/**
+ * Created by soyoon on 2016. 1. 30..
+ */
+public class GraphActivity {
+}
---
+++ RainbowCouple/app/src/main/java/com/nexters/rainbow/rainbowcouple/login/StartActivity.java
@@ -0,0 +1,16 @@
+package com.nexters.rainbow.rainbowcouple.login;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by soyoon on 2016. 1. 23..
+ */
+public class StartActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+}
