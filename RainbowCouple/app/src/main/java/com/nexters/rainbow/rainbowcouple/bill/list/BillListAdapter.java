262df403ad81d4e0cd7ce165d1d869d399a3a0ab
Soyoon <soyoon0918@gmail.com>
2016-02-14 14:48:14 +0900

    Bill List Adapter수정

--- RainbowCouple/app/src/main/java/com/nexters/rainbow/rainbowcouple/bill/list/BillListAdapter.java
+++ RainbowCouple/app/src/main/java/com/nexters/rainbow/rainbowcouple/bill/list/BillListAdapter.java
@@ -1,16 +1,19 @@
 package com.nexters.rainbow.rainbowcouple.bill.list;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.LinearLayout;
+import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.nexters.rainbow.rainbowcouple.R;
 import com.nexters.rainbow.rainbowcouple.bill.Bill;
 import com.nexters.rainbow.rainbowcouple.common.BaseAdapter;
+import com.nexters.rainbow.rainbowcouple.common.utils.CollectionUtils;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 
@@ -19,16 +22,49 @@ import butterknife.ButterKnife;
 
 public class BillListAdapter extends BaseAdapter<Bill, BillListAdapter.BillItemViewHolder> {
 
-    private final String FORMAT_BILL_BUDGET = "%,d";
+    private Context context;
+
+    private LayoutInflater layoutInflater;
+    private List<Bill> dataList;
+    private int layoutResourceId;
+
+    private final String FORMAT_BILL_BUDGET = "₩,d";
     private final DateFormat dateFormat = new SimpleDateFormat("yyyy.MM.dd HH:mm", Locale.KOREA);
 
     public BillListAdapter(Context context, int layoutResourceId, List<Bill> dataList) {
         super(context, layoutResourceId, dataList);
+        this.context = context;
+        this.dataList = dataList;
+        this.layoutResourceId = layoutResourceId;
+        this.layoutInflater = LayoutInflater.from(context);
     }
 
-    public BillItemViewHolder bindView(View parentView) {
-        return new BillItemViewHolder(parentView);
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        BillItemViewHolder viewHolder;
+
+        if (convertView != null) {
+            viewHolder = (BillItemViewHolder) convertView.getTag();
+        } else {
+            convertView = layoutInflater.inflate(layoutResourceId, parent, false);
+            viewHolder = bindView(convertView);
+            convertView.setTag(viewHolder);
+        }
+
+        loadView(convertView, viewHolder, getItem(position), position);
+
+        return convertView;
     }
+
+    public void addData(Bill data) {
+        if (dataList == null) {
+            dataList = new ArrayList<>();
+        }
+
+        dataList.add(data);
+        notifyDataSetChanged();
+    }
+
 
     @Override
     public void loadView(View convertView, BillItemViewHolder viewHolder, Bill item, int position) {
@@ -36,6 +72,29 @@ public class BillListAdapter extends BaseAdapter<Bill, BillListAdapter.BillItemV
         viewHolder.textViewBillComment.setText(item.getComment());
         viewHolder.textViewBillAmount.setText(String.format(FORMAT_BILL_BUDGET, item.getAmount()));
     }
+
+    @Override
+    public int getCount() {
+        return CollectionUtils.isEmpty(dataList) ? 0 : dataList.size();
+    }
+
+    @Override
+    public Bill getItem(int position) {
+        if (CollectionUtils.isEmpty(dataList) || position > dataList.size())
+            return null;
+        return dataList.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+
+    public BillItemViewHolder bindView(View parentView) {
+        return new BillItemViewHolder(parentView);
+    }
+
 
     class BillItemViewHolder {
         @Bind(R.id.textViewBillCategory) TextView textViewBillCategory;
